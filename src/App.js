2e51328958523e056cb7f4aa72c896184ffb8478
Kennidy Stillwell <you@example.com>
2024-01-07 18:10:21 -0500

    very easy challenge completeted

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  //very easy challenge
+  render() {
+    return (
+      <div className="App">
+        <h1>Name: Kennidy Stillwell</h1>
+        <p>Number: 704-456-7890</p>
+        <p>Date of Birth: August 18, 2003</p>
+        <p>Job: Coding Apprentice at R2H</p>
+      </div>
+    );
+  }
+}
+
+export default App;
