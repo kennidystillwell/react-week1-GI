144b9cb28c331155569bb600a9588d637d204715
Kennidy Stillwell <you@example.com>
2024-01-07 18:20:08 -0500

    easy challenge completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  //easy challenge
+  constructor(props) { 
+    super(props);
+    this.state = {
+      person: {}
+    };
+  }
+
+
+
   //very easy challenge
   render() {
     return (
+      //div containing basic info; hard coded
       <div className="App">
         <h1>Name: Kennidy Stillwell</h1>
         <p>Number: 704-456-7890</p>
